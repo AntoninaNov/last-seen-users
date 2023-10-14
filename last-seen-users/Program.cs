90eb94c0004f6c3c1bd85d039747ca2f2e04abd0
AntNov <tonianovak@yahoo.com>
2023-10-14 18:35:04 +0300

    Implemented Localizations, closes #6

--- last-seen-users/Program.cs
+++ last-seen-users/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Reflection;
 
 namespace LastSeenApplication
 {
@@ -9,52 +10,70 @@ namespace LastSeenApplication
     {
         static async Task Main(string[] args)
         {
-            List<(string Username, DateTime? LastSeen)> parsedData = new List<(string Username, DateTime? LastSeen)>();
+            // Ask user to select a language
+            Console.WriteLine("Select a language: [en] English, [ua] Ukrainian, [de] German, [es] Spanish, [fr] French");
+            string language = Console.ReadLine().ToLower();
 
-            int offset = 0;
-            LastSeenUserResult result;
+            // Fallback to English if input doesn't match any supported languages
+            if (!new[] { "en", "ua", "de", "es", "fr" }.Contains(language))
+            {
+                language = "en";
+            }
 
             try
             {
-                do
-                {
-                    result = await DataFetcher.FetchDataAsync(offset);
-
-                    if (result.Data == null || result.Data.Length == 0)
-                    {
-                        break;
-                    }
-    
-                    // Parsing the fetched data using ParseUserData
-                    var currentParsedData = DataParser.ParseUserData(result.Data);
-                    parsedData.AddRange(currentParsedData);
-
-                    offset += result.Data.Length;
-                }
-                while (true);
-
-                // Using TimeFormatter to make time human-readable
-                foreach (var data in parsedData)
-                {
-                    if (data.LastSeen == null)
-                    {
-                        // If the user is online
-                        Console.WriteLine($"{data.Username} is online.");
-                    }
-                    else
-                    {
-                        // If the user is not online
-                        string humanReadableTime = TimeFormatter.GetHumanReadableTime(data.LastSeen);
-                        Console.WriteLine($"{data.Username} was last seen {humanReadableTime}.");
-                    }
-                }
-
+                List<(string Username, DateTime? LastSeen)> parsedData = await LoadLastSeenDataAsync();
+                DisplayLastSeenData(parsedData, language); // Language set by user input
             }
             catch (HttpRequestException e)
             {
-                // Handling HttpRequestException
                 Console.WriteLine(e.Message);
             }
         }
+
+        public static async Task<List<(string Username, DateTime? LastSeen)>> LoadLastSeenDataAsync()
+        {
+            List<(string Username, DateTime? LastSeen)> parsedData = new List<(string Username, DateTime? LastSeen)>();
+            int offset = 0;
+            LastSeenUserResult result;
+
+            do
+            {
+                result = await DataFetcher.FetchDataAsync(offset);
+
+                if (result.Data == null || result.Data.Length == 0)
+                {
+                    break;
+                }
+
+                var currentParsedData = DataParser.ParseUserData(result.Data);
+                parsedData.AddRange(currentParsedData);
+
+                offset += result.Data.Length;
+            }
+            while (true);
+
+            return parsedData;
+        }
+
+        
+        static void DisplayLastSeenData(List<(string Username, DateTime? LastSeen)> parsedData, string language = "en")
+        {
+            foreach (var data in parsedData)
+            {
+                if (data.LastSeen == null)
+                {
+                    Console.WriteLine($"{data.Username} {LocalizationService.GetText("Online", language)}.");
+                }
+                else
+                {
+                    // The TimeFormatter now accepts the 'language' parameter to return localized time descriptions
+                    string humanReadableTime = TimeFormatter.GetHumanReadableTime(data.LastSeen, language);
+                    Console.WriteLine($"{data.Username} {LocalizationService.GetText("WasOnline", language)} {humanReadableTime}.");
+                }
+            }
+        }
+
+        
     }
 }
