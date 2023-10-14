dbc6bbe2cada359cb2af266c49fc6e912a8d02af
AntNov <tonianovak@yahoo.com>
2023-10-14 18:35:04 +0300

    Implemented Display LastSeen Data, closes #4

--- last-seen-users/Program.cs
+++ last-seen-users/Program.cs
@@ -9,51 +9,56 @@ namespace LastSeenApplication
     {
         static async Task Main(string[] args)
         {
-            List<(string Username, DateTime? LastSeen)> parsedData = new List<(string Username, DateTime? LastSeen)>();
-
-            int offset = 0;
-            LastSeenUserResult result;
-
             try
             {
-                do
-                {
-                    result = await DataFetcher.FetchDataAsync(offset);
-
-                    if (result.Data == null || result.Data.Length == 0)
-                    {
-                        break;
-                    }
-    
-                    // Parsing the fetched data using ParseUserData
-                    var currentParsedData = DataParser.ParseUserData(result.Data);
-                    parsedData.AddRange(currentParsedData);
-
-                    offset += result.Data.Length;
-                }
-                while (true);
-
-                // Using TimeFormatter to make time human-readable
-                foreach (var data in parsedData)
-                {
-                    if (data.LastSeen == null)
-                    {
-                        // If the user is online
-                        Console.WriteLine($"{data.Username} is online.");
-                    }
-                    else
-                    {
-                        // If the user is not online
-                        string humanReadableTime = TimeFormatter.GetHumanReadableTime(data.LastSeen);
-                        Console.WriteLine($"{data.Username} was last seen {humanReadableTime}.");
-                    }
-                }
-
+                List<(string Username, DateTime? LastSeen)> parsedData = await LoadLastSeenDataAsync();
+                DisplayLastSeenData(parsedData, "en"); // Language set to English for this example
             }
             catch (HttpRequestException e)
             {
-                // Handling HttpRequestException
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        public static async Task<List<(string Username, DateTime? LastSeen)>> LoadLastSeenDataAsync()
+        {
+            List<(string Username, DateTime? LastSeen)> parsedData = new List<(string Username, DateTime? LastSeen)>();
+            int offset = 0;
+            LastSeenUserResult result;
+
+            do
+            {
+                result = await DataFetcher.FetchDataAsync(offset);
+
+                if (result.Data == null || result.Data.Length == 0)
+                {
+                    break;
+                }
+
+                var currentParsedData = DataParser.ParseUserData(result.Data);
+                parsedData.AddRange(currentParsedData);
+
+                offset += result.Data.Length;
+            }
+            while (true);
+
+            return parsedData;
+        }
+
+        
+        static void DisplayLastSeenData(List<(string Username, DateTime? LastSeen)> parsedData, string language = "en")
+        {
+            foreach (var data in parsedData)
+            {
+                if (data.LastSeen == null)
+                {
+                    Console.WriteLine($"{data.Username} {Localization.GetText("Online", language)}.");
+                }
+                else
+                {
+                    string humanReadableTime = TimeFormatter.GetHumanReadableTime(data.LastSeen);
+                    Console.WriteLine($"{data.Username} {Localization.GetText("WasOnline", language)} {humanReadableTime}.");
+                }
             }
         }
     }
