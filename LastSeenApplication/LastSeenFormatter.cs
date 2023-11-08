509449b3227e04a6acceaebc6c9fbe0348838e3b
AntNov <tonianovak@yahoo.com>
2023-11-08 22:49:26 +0200

    #3 Formatting time into readable strings

---
+++ LastSeenApplication/LastSeenFormatter.cs
@@ -0,0 +1,57 @@
+﻿namespace LastSeenApplication;
+
+public class LastSeenFormatter
+{
+    /// <summary>
+    /// Formats the last seen time relative to the current time.
+    /// </summary>
+    /// <param name="now">The current time.</param>
+    /// <param name="lastSeen">The time the user was last seen.</param>
+    /// <returns>A string representing how long ago the user was last seen.</returns>
+    public string Format(DateTimeOffset now, DateTimeOffset lastSeen)
+    {
+        TimeSpan span = now - lastSeen;
+        
+        if (span == TimeSpan.Zero)
+        {
+            return "Online";
+        }
+        
+        if (span < TimeSpan.FromSeconds(30))
+        {
+            return "Just Now";
+        }
+        
+        if (span < TimeSpan.FromMinutes(1))
+        {
+            return "Less than a minute ago";
+        }
+        
+        if (span < TimeSpan.FromMinutes(60))
+        {
+            return "Couple of minutes ago";
+        }
+        
+        if (span < TimeSpan.FromMinutes(120))
+        {
+            return "an hour ago";
+        }
+        
+        if (now.Date == lastSeen.Date)
+        {
+            return "today";
+        }
+        
+        if (now.Date - lastSeen.Date == TimeSpan.FromDays(1))
+        {
+            return "yesterday";
+        }
+        
+        if (span < TimeSpan.FromDays(7))
+        {
+            return "this week";
+        }
+        
+        return "long time ago";
+    }
+}
