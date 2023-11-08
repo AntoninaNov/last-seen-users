ff1f624d5ab8fdd19c87e583d9148add4bf1cbb8
AntNov <tonianovak@yahoo.com>
2023-11-08 22:58:24 +0200

    #4 Display user last-seen times list

---
+++ LastSeenApplication/LastSeenApplication.cs
@@ -0,0 +1,24 @@
+﻿namespace LastSeenApplication
+{
+    public class LastSeenApplication
+    {
+        private readonly UserLoader _userLoader;
+        
+        // Constructor to inject the UserLoader dependency
+        public LastSeenApplication(UserLoader userLoader)
+        {
+            _userLoader = userLoader;
+        }
+
+        // Method to show the last-seen status of all users
+        public List<string> Show(DateTimeOffset now)
+        {
+            var users = _userLoader.LoadAllUsers();
+            var formatter = new LastSeenFormatter();
+            var lastSeenStatusList = users.Select(u => 
+                $"{u.Nickname} {formatter.Format(now, u.LastSeenDate ?? now)}").ToList();
+
+            return lastSeenStatusList;
+        }
+    }
+}
