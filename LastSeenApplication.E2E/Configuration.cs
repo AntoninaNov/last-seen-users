46e75b323a22719830c569626a4c8cae6d0bb184
AntNov <tonianovak@yahoo.com>
2023-11-08 23:50:21 +0200

    E2E Tests Implementation (Assignment#4)#26

---
+++ LastSeenApplication.E2E/Configuration.cs
@@ -0,0 +1,19 @@
+namespace LastSeenApplication.E2E;
+
+public static class Configuration
+{
+    private const string DefaultUrl = "http://127.0.0.1:5079";
+
+    public static string BaseUrl
+    {
+        get
+        {
+            var env = Environment.GetEnvironmentVariable("VM_IP");
+            if (env != null)
+            {
+                return $"http://{env}";
+            }
+            return DefaultUrl;
+        }
+    }
+}
---
+++ LastSeenApplication.E2E/HttpHelper.cs
@@ -0,0 +1,17 @@
+using System.Net;
+
+namespace LastSeenApplication.E2E;
+
+
+public static class HttpHelper
+{
+    public static (HttpStatusCode statusCode, string response) Get(string apiUrl)
+    {
+        using var client = new HttpClient();
+        var url = new Uri(Configuration.BaseUrl + apiUrl);
+        using var result = client.Send(new HttpRequestMessage(HttpMethod.Get, url));
+        using var reader = new StreamReader(result.Content.ReadAsStream());
+
+        return (result.StatusCode, reader.ReadToEnd());
+    }
+}
